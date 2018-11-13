a34fa39c1ac88c687f03af083ecadfef5dc6b41d
Technerder <technerder@hotmail.com>
2018-11-13 16:49:28 -0500

    Capitalized "codevision" to "Codevision"

--- routes/index.js
+++ routes/index.js
@@ -58,7 +58,7 @@ router.post("/register", function(req, res){
             return res.redirect("/register");
         }
         passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome to codevision," + user.username + "!");
+            req.flash("success", "Welcome to Codevision," + user.username + "!");
             res.redirect("/");
         });
     })
